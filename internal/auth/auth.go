eed0abe646efaae6cb66db6d9843208f47bf3134
carlhavoc <mkowalsky87@gmail.com>
2025-07-10 14:47:42 -0700

    get oauth working

---
+++ internal/auth/auth.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+)
+
+const (
+	MaxAge = 86400 * 30
+	IsProd = false
+)
+
+func NewAuth() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	session_key := os.Getenv("SESSION_SECRET_KEY")
+	if session_key == "" {
+		log.Fatal("Session secret key is not set or empty.  Check .env file.")
+	}
+
+	googleClientId := os.Getenv("GOOGLE_CLIENT_ID")
+	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+
+	store := sessions.NewCookieStore([]byte(session_key))
+	store.MaxAge(MaxAge)
+
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true
+	store.Options.Secure = IsProd
+	store.Options.SameSite = http.SameSiteLaxMode
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(googleClientId, googleClientSecret, "http://localhost:8080/auth/google/callback"),
+	)
+}
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"sort"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/m-kowalsky/weigh-in/internal/auth"
+	"github.com/m-kowalsky/weigh-in/internal/database"
+	"github.com/markbates/goth/gothic"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -23,8 +30,21 @@ type Data struct {
 
 const tmpl_path = "templates/*.html"
 
+type apiConfig struct {
+	db           *database.Queries
+	access_token string
+}
+
+type User struct {
+	Id        string
+	Email     string
+	Firstname string
+}
+
 func main() {
 
+	providerIndex := gothProviderSetup()
+	auth.NewAuth()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
@@ -43,9 +63,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	db_queries := database.New(Db)
 
-	r.Get("/", handlerIndex)
-	r.Get("/up/", handlerKamalHealthCheck)
+	apiCfg := apiConfig{
+		db:           db_queries,
+		access_token: "",
+	}
+
+	// Routes
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
+		err = tmpl.ExecuteTemplate(w, "index.html", providerIndex)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+	r.Get("/up/", apiCfg.handlerKamalHealthcheck)
+	r.Get("/logout/{provider}", logoutProviderFunction)
+	r.Get("/auth/{provider}", handlerGetAuthProvider)
+	r.Get("/auth/{provider}/callback", handlerGetAuthCallback)
+
+	// Run server
 
 	err = http.ListenAndServe(":8080", r)
 	log.Println("Serving on port 8080....")
@@ -67,17 +107,67 @@ func parseHTMLTemplates(path string) error {
 
 }
 
-func handlerIndex(w http.ResponseWriter, _ *http.Request) {
-	data := Data{}
-
-	data.Title = "Stop sucking"
-	data.Body = "I am trying"
-
-	tmpl.ExecuteTemplate(w, "index", data)
-
-}
-
-func handlerKamalHealthCheck(w http.ResponseWriter, _ *http.Request) {
+func (apiCfg *apiConfig) handlerKamalHealthcheck(w http.ResponseWriter, _ *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func handlerGetAuthCallback(w http.ResponseWriter, r *http.Request) {
+	provider := chi.URLParam(r, "provider")
+	r = r.WithContext(context.WithValue(r.Context(), "provider", provider))
+
+	user, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		fmt.Println("Auth error:", err)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "user.html", user)
+
+	fmt.Println(user.UserID)
+	fmt.Println(user.Email)
+	fmt.Println(user.FirstName)
+	fmt.Printf("type of access token: %T", user.AccessToken)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func handlerGetAuthProvider(w http.ResponseWriter, r *http.Request) {
+
+	// try to get the user without re-authenticating
+	if gothUser, err := gothic.CompleteUserAuth(w, r); err == nil {
+		err := tmpl.ExecuteTemplate(w, "user.html", gothUser)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		gothic.BeginAuthHandler(w, r)
+	}
+}
+
+func logoutProviderFunction(w http.ResponseWriter, r *http.Request) {
+	gothic.Logout(w, r)
+	w.Header().Set("Location", "/")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
+}
+
+func gothProviderSetup() *ProviderIndex {
+
+	m := map[string]string{
+		"google": "Google",
+	}
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+
+	return providerIndex
 }
